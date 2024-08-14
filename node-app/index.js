3c19741fd1e0e8135eb7c187f031f36be4b7e16a
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <110688069+ana-pfeilsticker@users.noreply.github.com>
2024-08-14 08:40:10 -0300

    Update index.js

--- node-app/index.js
+++ node-app/index.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import * as User from "./controllers/UserMET.js";
 import * as Professor from "./controllers/ProfessorMET.js";
@@ -9,8 +10,16 @@ import * as Comentario from "./controllers/ComentarioMET.js";
 
 dotenv.config();
 
+const corsOptions = {
+  origin: process.env.URL_FRONT_END,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const sequelize = new Sequelize({
